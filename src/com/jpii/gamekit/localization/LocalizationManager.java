014e6ff0644161fddca17eb2287e4a17d6d3930d
TexasGamer <tpgaubert@gmail.com>
2012-12-15 12:37:08 -0600

    Load files

--- src/com/jpii/gamekit/localization/LocalizationManager.java
+++ src/com/jpii/gamekit/localization/LocalizationManager.java
@@ -1,44 +1,25 @@
 package com.jpii.gamekit.localization;
 
-import java.io.IOException;
 import java.net.URL;
-import java.security.CodeSource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class LocalizationManager {
 
 	private Locale currentLocale;
-	private String[] files;
+	private String langCode;
+	private URL defaultStrings;
+	private URL currentStrings;
 
-	public LocalizationManager(Class reference) {
-		CodeSource src = reference.getProtectionDomain().getCodeSource();
-		List<String> list = new ArrayList<String>();
-		
-		try {
-			if(src != null) {
-				URL jar = src.getLocation();
-				ZipInputStream zip = new ZipInputStream(jar.openStream());
-				ZipEntry ze = null;
-	
-				while((ze = zip.getNextEntry()) != null ) {
-					String entryName = ze.getName();
-					if(entryName.startsWith("images") && entryName.endsWith(".png") ) {
-						list.add(entryName);
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-			
-		files = list.toArray(new String[list.size()]);
-		
-		System.out.println(files[0]);
+	public LocalizationManager(Class reference, String dir) {
 		currentLocale = Locale.getDefault();
+		langCode = currentLocale.toString().substring(0, 2);
+		
+		defaultStrings = reference.getResource(dir + "/strings.xml");
+		currentStrings = reference.getResource(dir + "/strings-" + langCode + ".xml");
+		
+		if(langCode == "en") {
+			defaultStrings = currentStrings;
+		}
 	}
 
 	/**
@@ -47,5 +28,17 @@ public class LocalizationManager {
 	 */
 	public Locale getLocale() {
 		return currentLocale;
+	}
+	
+	public String getLanguageCode() {
+		return langCode;
+	}
+	
+	public URL getDefaultStringsUrl() {
+		return defaultStrings;
+	}
+	
+	public URL getCurrentStringsUrl() {
+		return currentStrings;
 	}
 }
--- src/com/jpii/gamekit/test/LocalizationTest.java
+++ src/com/jpii/gamekit/test/LocalizationTest.java
@@ -1,10 +1,9 @@
 package com.jpii.gamekit.test;
 
-import com.jpii.gamekit.debug.Debugger;
 import com.jpii.gamekit.localization.LocalizationManager;
 
 public class LocalizationTest {
 	public static void main(String[] args) {
-		LocalizationManager localizationManager = new LocalizationManager(LocalizationTest.class);
+		LocalizationManager localizationManager = new LocalizationManager(LocalizationTest.class, "/com/jpii/gamekit/test/res");
 	}
 }
