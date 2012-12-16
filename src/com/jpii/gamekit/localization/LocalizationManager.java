06f26e5524bd574446f7b07a5a886fba47b37adb
TexasGamer <tpgaubert@gmail.com>
2012-12-15 21:53:27 -0600

    Actually read files now (wip still)

--- src/com/jpii/gamekit/localization/LocalizationManager.java
+++ src/com/jpii/gamekit/localization/LocalizationManager.java
@@ -1,25 +1,85 @@
 package com.jpii.gamekit.localization;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.util.Locale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class LocalizationManager {
 
 	private Locale currentLocale;
 	private String langCode;
-	private URL defaultStrings;
-	private URL currentStrings;
+	private InputStream defaultStrings;
+	private InputStream currentStrings;
 
 	public LocalizationManager(Class reference, String dir) {
 		currentLocale = Locale.getDefault();
 		langCode = currentLocale.toString().substring(0, 2);
-		
-		defaultStrings = reference.getResource(dir + "/strings.xml");
-		currentStrings = reference.getResource(dir + "/strings-" + langCode + ".xml");
-		
-		if(langCode == "en") {
-			defaultStrings = currentStrings;
+
+		defaultStrings = reference.getResourceAsStream(dir + "/strings.xml");
+		currentStrings = reference.getResourceAsStream(dir + "/strings-" + langCode + ".xml");
+
+		if(langCode.equalsIgnoreCase("en")) {
+			currentStrings = defaultStrings;
 		}
+
+		Document doc = newDocumentFromInputStream(currentStrings);
+
+		NodeList resources = doc.getElementsByTagName("resources");
+
+		for (int i = 0; i < resources.getLength(); i++) {
+			Node firstNode = resources.item(i);
+
+			if (firstNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element element = (Element) firstNode;
+				NodeList stringElementList = element.getElementsByTagName("string");
+				Element stringElement = (Element) stringElementList.item(0);
+				NodeList string = stringElement.getChildNodes();
+				System.out.println("Value: " + string.item(0).getNodeValue() + " at " + string.item(0));
+
+			}
+		}
+	}
+
+	public static Document newDocumentFromInputStream(InputStream in) {
+		DocumentBuilderFactory factory = null;
+		DocumentBuilder builder = null;
+		Document ret = null;
+
+		try {
+			factory = DocumentBuilderFactory.newInstance();
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			ret = builder.parse(new InputSource(in));
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
+	public String getString(String key) {
+		return "";
 	}
 
 	/**
@@ -29,16 +89,16 @@ public class LocalizationManager {
 	public Locale getLocale() {
 		return currentLocale;
 	}
-	
+
 	public String getLanguageCode() {
 		return langCode;
 	}
-	
-	public URL getDefaultStringsUrl() {
+
+	public InputStream getDefaultStringsUrl() {
 		return defaultStrings;
 	}
-	
-	public URL getCurrentStringsUrl() {
+
+	public InputStream getCurrentStringsUrl() {
 		return currentStrings;
 	}
 }
