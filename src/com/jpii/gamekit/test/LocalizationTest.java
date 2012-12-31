b5ee543c66dd6f5ab1c8f56da0e86ace57bcea61
TexasGamer <tpgaubert@gmail.com>
2012-12-30 22:03:38 -0600

    Added proper tests

--- src/com/jpii/gamekit/test/LocalizationTest.java
+++ src/com/jpii/gamekit/test/LocalizationTest.java
@@ -1,11 +1,30 @@
 package com.jpii.gamekit.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.jpii.gamekit.debug.Command;
+import com.jpii.gamekit.debug.CommandAction;
 import com.jpii.gamekit.localization.LocalizationManager;
 
 public class LocalizationTest {
-	public static void main(String[] args) {
+	
+	@Test
+	public void testEnglishLocalization() {
 		LocalizationManager localizationManager = new LocalizationManager(LocalizationTest.class, "/com/jpii/gamekit/test/res");
-		System.out.println(localizationManager.getString("test_value"));
-		System.out.println(localizationManager.getString("en_only"));
+		
+		assertEquals("Result", "Hello", localizationManager.getString("test_value"));
+		assertEquals("Result", "This string is in English only", localizationManager.getString("en_only"));
+		assertEquals("Result", "undefined_string", localizationManager.getString("undefined_string"));
+	}
+	
+	@Test
+	public void testForeignLocalization() {
+		LocalizationManager localizationManager = new LocalizationManager(LocalizationTest.class, "/com/jpii/gamekit/test/res", "es");
+		
+		assertEquals("Result", "Hola", localizationManager.getString("test_value"));
+		assertEquals("Result", "This string is in English only", localizationManager.getString("en_only"));
+		assertEquals("Result", "undefined_string", localizationManager.getString("undefined_string"));
 	}
 }
