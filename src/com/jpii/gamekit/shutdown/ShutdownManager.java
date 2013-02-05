fb6aa7c8b6fe6535fc6fb113706ce8c57da7f36b
TexasGamer <tpgaubert@gmail.com>
2013-02-05 11:20:32 -0600

    Added Shutdown component docs and methods

--- src/com/jpii/gamekit/shutdown/ShutdownManager.java
+++ src/com/jpii/gamekit/shutdown/ShutdownManager.java
@@ -6,6 +6,10 @@ public class ShutdownManager {
 	
 	ArrayList<ShutdownRoutine> routines = new ArrayList<ShutdownRoutine>();
 	
+	/**
+	 * Run all <code>ShutdownRoutines</code> that have been registered, then exit the program.
+	 * Calls <code>onRoutineRun</code> and <code>onShutdown</code> events.
+	 */
 	public void runRoutines() {
 		for(ShutdownRoutine routine : routines) {
 			onRoutineRun(routine);
@@ -15,24 +19,52 @@ public class ShutdownManager {
 		shutdown();
 	}
 	
+	/**
+	 * Register a <code>ShutdownRoutine</code>. Each routine is called when <code>runRoutines</code>
+	 * method is called.
+	 * Calls <code>onRoutineRegistered</code> event.</code>
+	 * @param shutdownRoutine
+	 */
 	public void addRoutine(ShutdownRoutine shutdownRoutine) {
-		
+		onRoutineRegistered(shutdownRoutine);
+		routines.add(shutdownRoutine);
 	}
 	
+	/**
+	 * Returns all registered <code>ShutdownRoutines</code>.
+	 * @return
+	 */
 	public ArrayList<ShutdownRoutine> getRoutines() {
 		return routines;
 	}
 	
+	/**
+	 * Close the program without running <code>ShutdownRoutines</code>.
+	 * Calls <code>onShutdown</code> event.
+	 */
 	public void shutdown() {
 		onShutdown();
 		System.exit(0);
 	}
 	
+	/**
+	 * Event called when <code>shutdown</code> method is called.
+	 */
 	public void onShutdown() {
 		
 	}
 	
+	/**
+	 * Event called when <code>runRoutines</code> method is called.
+	 */
 	public void onRoutineRun(ShutdownRoutine shutdownRoutine) {
+		
+	}
+	
+	/**
+	 * Event called when <code>registerRoutine</code> method is called.
+	 */
+	public void onRoutineRegistered(ShutdownRoutine shutdownRoutine) {
 		
 	}
 }
--- src/com/jpii/gamekit/shutdown/ShutdownRoutine.java
+++ src/com/jpii/gamekit/shutdown/ShutdownRoutine.java
@@ -1,6 +1,10 @@
 package com.jpii.gamekit.shutdown;
 
 public class ShutdownRoutine {
+	/**
+	 * Event called when <code>ShutdownRoutine</code> is ran. 
+	 * Called by <code>ShutdownManager</code> with the <code>runRoutines</code> method.
+	 */
 	public void run() {
 		
 	}
