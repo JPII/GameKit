3a0c0a7d99a67c4a5e58bcd321354d15158e1c9e
TexasGamer <tpgaubert@gmail.com>
2013-02-07 13:19:11 -0600

    Added Toast notifications, fixes #4

---
+++ src/com/jpii/gamekit/toaster/SingleToaster.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2012 Daniele Piras
+ *
+ * Modified under license by JPII and contributors.
+ */
+
+package com.jpii.gamekit.toaster;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class SingleToaster extends javax.swing.JWindow {
+	private static final long serialVersionUID = 1L;
+
+	// Label to store Icon
+	public JLabel iconLabel = new JLabel();
+
+	// Text area for the message
+	public JTextArea message = new JTextArea();
+	
+	public Toaster t;
+
+	/***
+	 * Simple constructor that initialized components...
+	 */
+	public SingleToaster(Toaster t) {
+		this.t = t;
+		initComponents(t);
+	}
+
+	/***
+	 * Function to initialized components
+	 */
+	public void initComponents(Toaster t) {
+
+		setSize(t.getToasterWidth(), t.getToasterHeight());
+		message.setFont(t.getToasterMessageFont());
+		JPanel externalPanel = new JPanel(new BorderLayout(1, 1));
+		externalPanel.setBackground(t.getBorderColor());
+		
+		final Toaster finaly = t;
+		
+		@SuppressWarnings("serial")
+		JPanel innerPanel = new JPanel(new BorderLayout(t.getMargin(),
+				t.getMargin())) {
+			
+			
+			
+			@Override
+			public void paint(Graphics g) {
+				if (finaly.getBackgroundImage() != null) {
+					g.drawImage(finaly.getBackgroundImage(), 0, 0, null);
+				}
+				super.paint(g);
+			}
+		};
+		if (t.getBackgroundImage() != null) {
+			innerPanel.setOpaque(false);
+			message.setOpaque(false);
+			iconLabel.setOpaque(false);
+		}
+		innerPanel.setBackground(t.getToasterColor());
+		message.setBackground(t.getToasterColor());
+		message.setMargin(new Insets(2, 2, 2, 2));
+		message.setLineWrap(true);
+		message.setWrapStyleWord(true);
+		EtchedBorder etchedBorder = (EtchedBorder) BorderFactory
+				.createEtchedBorder();
+		externalPanel.setBorder(etchedBorder);
+		externalPanel.add(innerPanel);
+		message.setForeground(t.getMessageColor());
+		innerPanel.add(iconLabel, BorderLayout.WEST);
+		innerPanel.add(message, BorderLayout.CENTER);
+		getContentPane().add(externalPanel);
+	}
+
+	/***
+	 * Start toaster animation...
+	 */
+	public void animate() {
+		(new Animation(this)).start();
+	}
+
+}
