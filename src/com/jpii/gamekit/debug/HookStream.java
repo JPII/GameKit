742af808710de624fe79ee50b95224151fd4bea1
TexasGamer <tpgaubert@gmail.com>
2013-02-23 12:56:18 -0600

    Clean up HookStream

--- src/com/jpii/gamekit/debug/HookStream.java
+++ src/com/jpii/gamekit/debug/HookStream.java
@@ -14,43 +14,35 @@ import com.jpii.gamekit.GameKit;
 
 public class HookStream extends PrintStream {
 
-
 	public HookStream(OutputStream out) {
 		super(out);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(String fileName) throws FileNotFoundException {
 		super(fileName);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(File file) throws FileNotFoundException {
 		super(file);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(OutputStream out, boolean autoFlush) {
 		super(out, autoFlush);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(String fileName, String csn)
 			throws FileNotFoundException, UnsupportedEncodingException {
 		super(fileName, csn);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(File file, String csn) throws FileNotFoundException,
 			UnsupportedEncodingException {
 		super(file, csn);
-		// TODO Auto-generated constructor stub
 	}
 
 	public HookStream(OutputStream out, boolean autoFlush, String encoding)
 			throws UnsupportedEncodingException {
 		super(out, autoFlush, encoding);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void println(String s) {
@@ -60,5 +52,4 @@ public class HookStream extends PrintStream {
 			GameKit.debug.printInfo(s);
 		}
 	}
-
 }
